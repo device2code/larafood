6549d4fcfc6cc619119e7ff9e19b749b924232ca
André C. Scherrer <andrescherrer@gmail.com>
2021-07-25 17:54:40 -0300

    Adiciona validacao dos campos dos planos

--- app/Http/Controllers/Admin/PlanController.php
+++ app/Http/Controllers/Admin/PlanController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUpdatePlan;
 use App\Models\Plan;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
@@ -30,7 +31,7 @@ class PlanController extends Controller
         return view('admin.pages.plans.create');
     }
 
-    public function store(Request $request)
+    public function store(StoreUpdatePlan $request)
     {
         $data = $request->all();
         $data['price'] = str_replace(',','.',$data['price']);
@@ -64,7 +65,7 @@ class PlanController extends Controller
         ]);
     }
 
-    public function update(Request $request, $url)
+    public function update(StoreUpdatePlan $request, $url)
     {
         $plan = $this->repository->where('url', $url)->first();
 
